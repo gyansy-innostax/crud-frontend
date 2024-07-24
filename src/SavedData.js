cec93b112102a1f1b703d549b56b1884095e702f
Gyan <gyan.yadav@innostax.com>
2024-07-24 11:41:08 +0530

    updated

--- src/SavedData.js
+++ src/SavedData.js
@@ -1,62 +1,65 @@
-import './SavedData.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchedAllData, updateInitial, deleteUsersData } from './features/crudSlice';
+import "./SavedData.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchedAllData,
+  updateInitial,
+  deleteUsersData,
+} from "./features/crudSlice";
 import { MdDeleteForever } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const SavedData = () => {
-    const dispatch = useDispatch();
-    
-    useEffect(()=>{
-        dispatch(fetchedAllData());
-    },[])
+  const dispatch = useDispatch();
 
-    const usersData = useSelector(state => state.userData);
+  useEffect(() => {
+    dispatch(fetchedAllData());
+  },[]);
 
+  const usersData = useSelector((state) => state.userData);
 
-    return (
-        <>
-            <div className='heading'>
-                <h2>Saved Data</h2>
-            </div>
-            <div className="table">
-                <table>
-                    <thead className="table-heading">
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody className="table-body">
-                        {usersData.map((user,index) => (
-                            <tr key={index}>
-                                <td className='table-text'>{user.name}</td>
-                                <td className='table-text'>{user.email}</td>
-                                <td className='table-text'>{user.phone}</td>
-                                <td>
-                                    <MdDeleteForever
-                                        size="1.6rem"
-                                        cursor="pointer"
-                                        onClick={() => dispatch(deleteUsersData(user))}
-                                    />
-                                </td>
-                                <td>
-                                    <FaEdit
-                                        size="1.3rem"
-                                        cursor="pointer"
-                                        onClick={()=> dispatch(updateInitial(user))}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="heading">
+        <h2>Saved Data</h2>
+      </div>
+      <div className="table">
+        <table>
+          <thead className="table-heading">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody className="table-body">
+            {usersData.map((user, index) => (
+              <tr key={index}>
+                <td className="table-text">{user.name}</td>
+                <td className="table-text">{user.email}</td>
+                <td className="table-text">{user.phone}</td>
+                <td>
+                  <MdDeleteForever
+                    size="1.6rem"
+                    cursor="pointer"
+                    onClick={() => dispatch(deleteUsersData(user))}
+                  />
+                </td>
+                <td>
+                  <FaEdit
+                    size="1.3rem"
+                    cursor="pointer"
+                    onClick={() => dispatch(updateInitial(user))}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default SavedData;
--- src/features/crudSlice.js
+++ src/features/crudSlice.js
@@ -2,118 +2,127 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    userData: [],
-    inputFormUpdate : {_id:'',name:'',phone:'',email:''}
-}
+  userData: [],
+  inputFormUpdate: { _id: "", name: "", phone: "", email: "" },
+};
 
 export const fetchedAllData = createAsyncThunk(
-    'user/fetchedData',
-    async () => {
-        const res = await axios('http://localhost:3000/api/users')
-        const data = await res.data;
-        return data
-    }
-)
+  "user/fetchedAllData",
+  async () => {
+    const res = await axios("http://localhost:3000/api/users");
+    const data = await res.data;
+    return data;
+  }
+);
 
 export const updateUserData = createAsyncThunk(
-    'user/updateUserData',
-    async (user) => {
-        const config = { 'content-type': 'application/json' };
-        const { _id } = user;
-        axios.put(`http://localhost:3000/api/user/${_id}`,user,config)
-        return user
-    }
-)
+  "user/updateUserData",
+  async (user) => {
+    const config = { "content-type": "application/json" };
+    const { _id } = user;
+    axios.put(`http://localhost:3000/api/user/${_id}`, user, config);
+    return user;
+  }
+);
 
 export const deleteUsersData = createAsyncThunk(
-    'user/deleteUsersData',
-    async (user) => {
-        const { _id } = user;
-        axios.delete(`http://localhost:3000/api/user/${_id}`)
-        return user
-    }
-)
+  "user/deleteUsersData",
+  async (user) => {
+    const { _id } = user;
+    axios.delete(`http://localhost:3000/api/user/${_id}`);
+    return user;
+  }
+);
 
 export const addUserData = createAsyncThunk(
-    'user/addUserData',
-    async (user) => {
-        const config = { 'content-type': 'application/json' };
-        axios.post(`http://localhost:3000/api/users`,user, config)
-            .then((res) => {
-                console.log(res.data)
-            }).catch((e)=>console.log(e))
-        return user
-    }
-)
+  "user/addUserData",
+  async (user) => {
+    const config = { "content-type": "application/json" };
+    axios
+      .post(`http://localhost:3000/api/users`, user, config)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => console.log(e));
+    return user;
+  }
+);
 
 export const crudSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        addUser: (state, action) => {
-            const user = {
-                name: action.payload.name,
-                phone: action.payload.phone,
-                email: action.payload.email
-            }
-            state.userData.push(user)
-        },
-        removeUser: (state, action) => {
-            state.userData = state.userData.filter((user) =>
-                user._id !== action.payload._id)
-        },
-        updateInitial: (state, action) => {
-            state.inputFormUpdate = {
-                _id: action.payload._id,
-                name: action.payload.name,
-                phone: action.payload.phone,
-                email: action.payload.email
-            }
-        },
-        updateUser: (state, action) => {
-            state.userData = state.userData.map((user) => {
-                if(user._id === action.payload._id){
-                    const data = {
-                        name: action.payload.name,
-                        phone: action.payload.phone,
-                        email: action.payload.email
-                    }
-                    return data;
-                }
-                else return user
-            })
-        }
+  name: "user",
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      const { name, phone, email } = action.payload;
+      const user = {
+        name,
+        phone,
+        email,
+      };
+      state.userData.push(user);
     },
-    extraReducers: (builder) => {
-        builder.addCase(fetchedAllData.fulfilled, (state,action) => {
-            state.userData = [...state.userData,...action.payload]
-        });
-        builder.addCase(deleteUsersData.fulfilled, (state,action) => {
-            state.userData = state.userData.filter((user) =>
-                user._id !== action.payload._id)
-        });
-        builder.addCase(addUserData.fulfilled, (state,action) => {
-            state.userData.push({
-                name: action.payload.name,
-                phone: action.payload.phone,
-                email: action.payload.email
-            })
-        });
-        builder.addCase(updateUserData.fulfilled, (state,action)=>{
-            state.userData = state.userData.map((user) => {
-                if(user._id === action.payload._id){
-                    const data = {
-                        name: action.payload.name,
-                        phone: action.payload.phone,
-                        email: action.payload.email
-                    }
-                    return data;
-                }
-                else return user
-            })
-        })
-    }
-})
+    removeUser: (state, action) => {
+      state.userData = state.userData.filter(
+        (user) => user._id !== action.payload._id
+      );
+    },
+    updateInitial: (state, action) => {
+      const { _id, name, phone, email } = action.payload;
+      state.inputFormUpdate = {
+        _id,
+        name,
+        phone,
+        email,
+      };
+    },
+    updateUser: (state, action) => {
+      state.userData = state.userData.map((user) => {
+        if (user._id === action.payload._id) {
+          const { name, phone, email } = action.payload;
+          const data = {
+            name,
+            phone,
+            email,
+          };
+          return data;
+        } else return user;
+      });
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchedAllData.fulfilled, (state, action) => {
+      state.userData = [...state.userData, ...action.payload];
+    });
+    builder.addCase(deleteUsersData.fulfilled, (state, action) => {
+      state.userData = state.userData.filter(
+        (user) => user._id !== action.payload._id
+      );
+    });
+    builder.addCase(addUserData.fulfilled, (state, action) => {
+      const { name, phone, email } = action.payload;
+      state.userData.push({
+        name,
+        phone,
+        email,
+      });
+    });
+    builder.addCase(updateUserData.fulfilled, (state, action) => {
+      state.userData = state.userData.map((user) => {
+        if (user._id === action.payload._id) {
+          const { name, phone, email } = action.payload;
+          const data = {
+            name,
+            phone,
+            email,
+          };
+          return data;
+        } else return user;
+      });
+    });
+  },
+});
 
-export const { addUser, removeUser, updateInitial, updateUser } = crudSlice.actions
-export default crudSlice.reducer
+export const { addUser, removeUser, updateInitial, updateUser } =
+  crudSlice.actions;
+
+export default crudSlice.reducer;
